777008f19cbb5b5173a1546ccb2ca8c12b56a2d6
Thiago <thiagopalacios@eng.ci.ufpb.br>
2021-10-22 22:11:23 -0300

    feat: adding api url | adding loading and toasting components

---
+++ src/components/LoadingIndicator/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Loading} from './styles';
+
+const LoadingIndicator: React.FC = () => {
+  return (
+    <Container>
+      <Loading />
+    </Container>
+  );
+};
+
+export default LoadingIndicator;
